14c33304f39d7afce0d44980a6e790229cd02c9b
Matthew Herman <boomtownwhale@gmail.com>
2022-10-16 10:38:09 -0500

    Made some light progress towards a better understanding of Godot's 3D Physics systems
    I'm having trouble making an explosion that affects nearby RigidBodies (RBs). So far I have tried using a simple Area that acts like a reverse-black hole, with the Area's origin acting as the center point. The Area has a strong repelling effect, throwing any RBs away from it. The issue is that I can't seem to get the RBs to consistently, often not being pushed at all.
    
    I think I'll try getting any overlapping RBs in the Area's _physics_process(), and adding some impulse to them. Might be more controllable.
    
    I'll look into this more later.

--- triggers/tripwire.gd
+++ triggers/tripwire.gd
@@ -1,23 +1,13 @@
-# boom_barrel.gd
-
-extends RigidBody
+extends RayCast
 
 export (PackedScene) var explosion_scene = preload("res://physics_explosion/PhysExplosion.tscn")
 
-func _on_BoomBarrel_body_entered(body):
-	if body.is_in_group("hurtful"):
+func _physics_process(delta):
+	if get_collider():
 		boom()
-
-
-func _on_Area_area_entered(area):
-	if area.is_in_group("hurtful"):
-		boom()
-
-
+		
 func boom():
 	var new_explosion = explosion_scene.instance()
 	get_parent_spatial().add_child(new_explosion)
 	new_explosion.global_transform = self.global_transform
 	self.queue_free()
-
-
